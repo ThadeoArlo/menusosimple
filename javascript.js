3acec3f91a91c41fc89c9b18962268369f88a063
Thadeo Arlo <thadeoarlo@gmail.com>
2016-07-25 21:43:07 +0700

    Add files via upload
    individual css/ js file

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+	  function openMenu() {
+	    document.getElementById("menu").style.height = "100%";
+	  }
+	
+	  function closeMenu() {
+	    document.getElementById("menu").style.height = "0%";
+	  }
